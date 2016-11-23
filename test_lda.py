48bf9623ef70416e8b3d4bc2e6ec0156c02cc9d0
aquastar <qqadd520@126.com>
2016-11-22 23:57:58 -0500

    update

--- test_lda.py
+++ test_lda.py
@@ -3,3 +3,4 @@ from LDAModel import LDAModel
 if __name__ == '__main__':
     a = LDAModel(alpha=.01, beta=0.01, NumberOfIterations=1000, SaveStep=100, K=10)
     a.ModelInit('line_news')
+    a.estimate()
