2d575d4d1a54b7f0539dc07afda6780c329c0420
aquastar <aquastar@qq.com>
2016-12-04 22:47:06 -0500

    update

--- 1st_tags_extractor.py
+++ 1st_tags_extractor.py
@@ -1,13 +1,17 @@
 import glob
 import os
 import cPickle as pk
+from gensim.models.word2vec import Word2Vec as w
 
 file_list = glob.glob('./tags/*')
+tag_name_pool = set()
+tag_name_w2v = {}
 
 pic_tags = {}
 for file_name in file_list:
     f = open(file_name, 'r')
-    tag_name = file_name.split(os.sep)[-1].split('.')[0]
+    tag_name = file_name.split(os.sep)[-1].split('.')[0].split('_')[0]
+    tag_name_pool.add(tag_name)
     for line in f:
         cur_num = int(line.strip())
         if cur_num in pic_tags:
@@ -16,3 +20,9 @@ for file_name in file_list:
             pic_tags[cur_num] = [tag_name]
 
 pk.dump(pic_tags, open('pic_tags.pk', 'wb'))
+
+model = w.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+for t in tag_name_pool:
+    tag_name_w2v[t] = model[t]
+
+pk.dump(pic_tags, open('w2v_tags.pk', 'wb'))
