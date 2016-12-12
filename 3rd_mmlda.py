8bd51aa517e51dd0c1d2e93565aaa5744ed1eba3
aquastar <aquastar@qq.com>
2016-12-12 18:26:21 -0500

    update

--- 3rd_mmlda.py
+++ 3rd_mmlda.py
@@ -7,11 +7,21 @@
 
 import numpy
 import sys
+from random import shuffle, sample
+from scipy.misc import logsumexp
+from math import e
 from operator import add
 import cPickle as pk
-
 import time
 from sklearn.metrics.pairwise import cosine_similarity
+
+
+def log_softmax(vec):
+    return vec - logsumexp(vec)
+
+
+def softmax(vec):
+    return numpy.exp(log_softmax(vec))
 
 
 def refresh_output(data):
@@ -34,7 +44,7 @@ class LDA:
         self.K = K
         self.alpha = alpha  # parameter of topics prior
         self.beta = beta  # parameter of words prior
-        self.docs = docs
+        self.docs = numpy.array(docs) + 1
         self.V = 0
         self.Vw = V[0]
         self.Vi = V[1]
@@ -54,24 +64,26 @@ class LDA:
         self.n_zi = numpy.zeros(K) + self.Vi * beta  # word count of each topic
 
         # topic initialization
+        docs = self.docs
         print 'topic init'
         for m, doc in enumerate(docs):
             refresh_output(['initializing', m])
             self.N += len(doc)
-            zw_n = []
 
             # initialize word and images separately
+            zw_n = []
             for t in xrange(self.word_dim):
                 if smartinit:
                     p_zw = self.n_zw_t[:, t] * self.n_m_zw[m] / self.n_zw
                     zw = numpy.random.multinomial(1, p_zw / p_zw.sum()).argmax()
                 else:
                     zw = numpy.random.randint(0, K)
+                discount_word_weight = doc[0][t]
 
                 zw_n.append(zw)
-                self.n_m_zw[m, zw] += 1
-                self.n_zw_t[zw, t] += 1
-                self.n_zw[zw] += 1
+                self.n_m_zw[m, zw] += discount_word_weight
+                self.n_zw_t[zw, t] += discount_word_weight
+                self.n_zw[zw] += discount_word_weight
             self.zw_m_n.append(numpy.array(zw_n))
 
             zi_n = []
@@ -82,17 +94,18 @@ class LDA:
                 else:
                     zi = numpy.random.randint(0, K)
 
-                zi_n.append(zi)
-                self.n_m_zi[m, zi] += 1
-                self.n_zi_t[zi, t] += 1
-                self.n_zi[zi] += 1
+                discount_word_weight = doc[1][t]
 
+                zi_n.append(zi)
+                self.n_m_zi[m, zi] += discount_word_weight
+                self.n_zi_t[zi, t] += discount_word_weight
+                self.n_zi[zi] += discount_word_weight
             self.zi_m_n.append(numpy.array(zi_n))
         print ''
 
     def inference(self):
         """learning once iteration"""
-        for m, doc in enumerate(self.docs):
+        for m, doc in enumerate(sample(self.docs, 100)):
             refresh_output(['current doc', m])
             zw_n = self.zw_m_n[m]
             n_m_zw = self.n_m_zw[m]
@@ -101,113 +114,91 @@ class LDA:
 
             for t in xrange(self.word_dim):
                 # discount for n-th word t with topic z
+                discount_word_weight = doc[0][t]
+
                 zw = zw_n[t]
-                n_m_zw[zw] -= 1
-                self.n_zw_t[zw, t] -= 1
-                self.n_zw[zw] -= 1
+                n_m_zw[zw] -= discount_word_weight
+                self.n_zw_t[zw, t] -= discount_word_weight
+                self.n_zw[zw] -= discount_word_weight
 
                 # sampling topic new_z for t
                 p_zw = self.n_zw_t[:, t] * n_m_zw / self.n_zw
-                new_zw = numpy.random.multinomial(1, p_zw / p_zw.sum()).argmax()
+                new_zw = numpy.random.multinomial(1, softmax(p_zw / p_zw.sum())).argmax()
 
                 # set z the new topic and increment counters
                 zw_n[t] = new_zw
-                n_m_zw[new_zw] += 1
-                self.n_zw_t[new_zw, t] += 1
-                self.n_zw[new_zw] += 1
+                n_m_zw[new_zw] += discount_word_weight
+                self.n_zw_t[new_zw, t] += discount_word_weight
+                self.n_zw[new_zw] += discount_word_weight
 
             for t in xrange(self.image_dim):
                 # discount for n-th word t with topic z
+                discount_word_weight = doc[1][t]
+
                 zi = zi_n[t]
-                n_m_zi[zi] -= 1
-                self.n_zi_t[zi, t] -= 1
-                self.n_zi[zi] -= 1
+                n_m_zi[zi] -= discount_word_weight
+                self.n_zi_t[zi, t] -= discount_word_weight
+                self.n_zi[zi] -= discount_word_weight
 
                 # sampling topic new_z for t
                 p_zi = self.n_zi_t[:, t] * n_m_zi / self.n_zi
-                new_zi = numpy.random.multinomial(1, p_zi / p_zi.sum()).argmax()
+                new_zi = numpy.random.multinomial(1, softmax(p_zi / p_zi.sum())).argmax()
 
                 # set z the new topic and increment counters
                 zi_n[t] = new_zi
-                n_m_zi[new_zi] += 1
-                self.n_zi_t[new_zi, t] += 1
-                self.n_zi[new_zi] += 1
+                n_m_zi[new_zi] += discount_word_weight
+                self.n_zi_t[new_zi, t] += discount_word_weight
+                self.n_zi[new_zi] += discount_word_weight
 
     def worddist(self):
         """get topic-word distribution"""
-        return self.n_zw_t / self.n_zw[:, numpy.newaxis]
+        return self.n_zw_t / self.n_zw[:, numpy.newaxis], self.n_zi_t / self.n_zi[:, numpy.newaxis]
 
     def perplexity(self, docs=None):
-        if docs == None: docs = self.docs
+        if docs == None:
+            docs = self.docs
         phi = self.worddist()
-        log_per = 0
-        N = 0
+        log_per_1 = 0
+        log_per_2 = 0
+        N_1 = 0
+        N_2 = 0
         Kalpha = self.K * self.alpha
-        for m, doc in enumerate(docs):
-            theta = self.n_m_zw[m] / (len(self.docs[m]) + Kalpha)
-            for w in doc:
-                log_per -= numpy.log(numpy.inner(phi[:, w], theta))
-            N += len(doc)
-        return numpy.exp(log_per / N)
+        for m, doc in enumerate(numpy.array(docs)[:, 0]):
+            theta = self.n_m_zw[m] / (sum(doc) + Kalpha)
+            for w in xrange(len(doc)):
+                log_per_1 -= numpy.log(numpy.inner(phi[0][:, w], theta))
+            N_1 += sum(doc)
+
+        for m, doc in enumerate(numpy.array(docs)[:, 1]):
+            theta = self.n_m_zi[m] / (sum(doc) + Kalpha)
+            for w in xrange(len(doc)):
+                log_per_2 -= numpy.log(numpy.inner(phi[1][:, w], theta))
+            N_2 += sum(doc)
+        return numpy.exp(log_per_1 / N_1), numpy.exp(log_per_2 / N_2)
 
 
 def lda_learning(lda, iteration, voca=None):
-    # pre_perp = lda.perplexity()
-    # print ("initial perplexity=%f" % pre_perp)
     print 'topic training'
     # start = time.time()
 
     for i in range(iteration):
-        refresh_output(['training', i])
-        print 'training', i
-        lda.inference()
-        # perp = lda.perplexity()
-        # print ("-%d p=%f" % (i + 1, perp))
-        # if pre_perp:
-        #     if pre_perp < perp:
-        #         output_word_topic_dist(lda, voca)
-        #         pre_perp = None
-        #     else:
-        #         pre_perp = perp
-        #         output_word_topic_dist(lda, voca)
-
         if i == 0:
             print 'init topic distribution'
-            output_word_topic_dist(lda)
-    # end = time.time()
-    # print 'training time:', end - start
-    # print ''
+            output_word_topic_dist(lda, str(i))
+
+        lda.inference()
+
+        if i % 50 == 0:
+            perp = lda.perplexity()
+            print ("-%d p=%f, %f" % (i + 1, perp[0], perp[1]))
+
     print 'final topic distribution'
-    output_word_topic_dist(lda)
+    output_word_topic_dist(lda, str(i))
 
 
-def output_word_topic_dist(lda, top_N=5):
-    numpy.save('word_topic.npy', lda.n_zw_t)
-    numpy.save('img_topic.npy', lda.n_zi_t)
-
-    tag_name_w2v = pk.load(open('w2v_tags.pk', 'rb'))
-    for x in lda.n_zw_t:
-        top_words = [''] * top_N
-        top_words_sim = [.0] * top_N
-        for t, vec in tag_name_w2v.iteritems():
-            sim = cosine_similarity([normalize(x), normalize(vec)])[0][1]
-            if sim > min(top_words_sim):
-                index_to_replace = numpy.array(top_words_sim).argmin()
-                top_words[index_to_replace] = t
-                top_words_sim[index_to_replace] = sim
-        print top_words, top_words_sim
-
-    img_feats = pk.load(open('pic_dict.pk', 'rb'))
-    for x in lda.n_zi_t:
-        top_words = [''] * top_N
-        top_words_sim = [.0] * top_N
-        for t, vec in img_feats.iteritems():
-            sim = cosine_similarity([normalize(x), normalize(vec)])[0][1]
-            if sim > min(top_words_sim):
-                index_to_replace = numpy.array(top_words_sim).argmin()
-                top_words[index_to_replace] = t
-                top_words_sim[index_to_replace] = sim
-        print top_words, top_words_sim
+def output_word_topic_dist(lda, prefix):
+    numpy.save(prefix + '_word_topic.npy', lda.n_zw_t)
+    numpy.save(prefix + '_img_topic.npy', lda.n_zi_t)
 
 
 def main():
@@ -216,11 +207,11 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option("-f", dest="filename", help="corpus filename", default='complete_document_one_2_one.pk')
     parser.add_option("-c", dest="corpus", help="using range of Brown corpus' files(start:end)")
-    parser.add_option("--alpha", dest="alpha", type="float", help="parameter alpha", default=1)
-    parser.add_option("--beta", dest="beta", type="float", help="parameter beta", default=0.5)
+    parser.add_option("--alpha", dest="alpha", type="float", help="parameter alpha", default=0.1)
+    parser.add_option("--beta", dest="beta", type="float", help="parameter beta", default=0.1)
     parser.add_option("-k", dest="K", type="int", help="number of topics", default=10)
-    parser.add_option("-i", dest="iteration", type="int", help="iteration count", default=50)
-    parser.add_option("-s", dest="smartinit", action="store_true", help="smart initialize of parameters", default=False)
+    parser.add_option("-i", dest="iteration", type="int", help="iteration count", default=500)
+    parser.add_option("-s", dest="smartinit", action="store_true", help="smart initialize of parameters", default=True)
     parser.add_option("--stopwords", dest="stopwords", help="exclude stop words", action="store_true",
                       default=False)
     parser.add_option("--seed", dest="seed", type="int", help="random seed")
@@ -233,10 +224,6 @@ def main():
         numpy.random.seed(options.seed)
 
     lda = LDA(options.K, options.alpha, options.beta, corpus, [300, 1000], options.smartinit)
-
-    # import cProfile
-    # cProfile.runctx('lda_learning(lda, options.iteration, voca)', globals(), locals(), 'lda.profile')
-    # output_word_topic_dist(lda, voca)
     lda_learning(lda, options.iteration)
 
 
---
+++ 4th_analysis.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cPickle as pk
+from sklearn.metrics.pairwise import cosine_similarity
+
+n_zw_t = np.load('10t_word_topic.npy')
+n_zi_t = np.load('10t_img_topic.npy')
+
+top_N = 5
+
+tag_name_w2v = pk.load(open('w2v_tags.pk', 'rb'))
+for x in n_zw_t:
+    top_dim = x.argmax()
+
+    tkeys = np.array(tag_name_w2v.keys())
+    tvalues = np.array(tag_name_w2v.values())
+
+    top_N_words_index = np.array(tvalues)[:, top_dim].argsort()
+
+    print tkeys[top_N_words_index][::-1][:top_N], tvalues[:, top_dim][top_N_words_index][::-1][:top_N]
+
+img_feats = pk.load(open('pic_dict.pk', 'rb'))
+for x in n_zi_t:
+    top_dim = x.argmax()
+
+    tkeys = np.array(img_feats.keys())
+    tvalues = np.array(img_feats.values())
+
+    top_N_words_index = np.array(tvalues)[:, top_dim].argsort()
+
+    print tkeys[top_N_words_index][::-1][:top_N], tvalues[:, top_dim][top_N_words_index][::-1][:top_N]
